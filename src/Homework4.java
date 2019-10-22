df49e1839a152af2be2681c9a411b538ed6a791e
funny2joy <funny2joy@naver.com>
2019-10-22 21:48:18 +0900

    Java 7th

---
+++ src/Homework4.java
@@ -0,0 +1,18 @@
+public class Homework4 {
+    /*
+    숙제 4. 아래와 같은 수열이 존재한다.
+
+         1 1 3 4 5 8 12 17 25 37 ...
+
+         키보드 입력을 받아 몇 번 째 항까지
+         합치기(sigma) 를 수행할지 정한다.
+
+         합치기 결과를 출력하시오.
+     */
+
+    public static void main(String[] args) {
+        int Afirst = 1;
+        int Asecond = 1;
+        int Athird = 3;
+    }
+}
---
+++ src/Prov1Ans.java
@@ -0,0 +1,35 @@
+public class Prov1Ans {
+     /*문제. while 문의 사용해서
+         1 ~100 까지 숫자 중 3의 배수를 추출하시오
+         (for 문 사용 금지)    */
+     public static void main(String[] args) {
+         int i = 1;
+
+         /* for 의 경우
+            for(i = 1 ; i < 101; i++)
+            while (i ++ < 101)
+          */
+         while (i ++ < 101){
+
+             if(i % 3 == 0){
+                 System.out.println("i=" + i);
+             }
+         }
+        /*
+         for(i = 1 ; i++ < 101;){// 증감부를 빼고 조건을 넣을 수 있다.
+             if(i % 3 == 0){
+                 System.out.println("i=" + i);
+             }
+         }
+
+         */
+        i = 1;
+
+        while(i < 101){
+            if(i % 3 == 0){
+                System.out.println("i=" + i);
+            }
+            i++ ;
+        }
+     }
+}
